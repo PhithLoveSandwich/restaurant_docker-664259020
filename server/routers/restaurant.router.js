cd76285bf45a862a79eb6dd4cf600f2fb3c5ffb3
PhithLoveSandwich <664259020@webmail.npru.ac.th>
2025-07-17 11:59:56 +0700

    อัพเดท Method

--- server/routers/restaurant.router.js
+++ server/routers/restaurant.router.js
@@ -5,5 +5,12 @@ const router = express.Router();
 
 // POST https://localhost:5000/api/v1/restaurant
 router.post("/", restaurantController.create);
-
+// GET https://localhost:5000/api/v1/restaurant
+router.get("/",restaurantController.getAll);
+// GET https://localhost:5000/api/v1/restaurant:id
+router.get("/:id",restaurantController.getById);
+// PUT https://localhost:5000/api/v1/restaurant/:id
+router.put("/:id", restaurantController.update);
+// DELETE https://localhost:5000/api/v1/restaurant/:id
+router.delete("/:id", restaurantController.deleteById)
 export default router; // แก้ชื่อให้ตรงกับที่ประกาศไว้
